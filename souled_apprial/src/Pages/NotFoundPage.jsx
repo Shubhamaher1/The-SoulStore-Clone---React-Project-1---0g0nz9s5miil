9c4f78ed4083a05ff491514438590ba6a86c9587
VivekTomar03 <tomarvivek90viv@gmail.com>
2023-02-26 20:28:42 +0530

    final commit1

--- souled_apprial/src/Pages/NotFoundPage.jsx
+++ souled_apprial/src/Pages/NotFoundPage.jsx
@@ -1,10 +1,11 @@
+import { Box, Image } from '@chakra-ui/react';
 import React from 'react';
 
 const NotFoundPage = () => {
   return (
-    <div>
-      Not found page
-    </div>
+    <Box marginLeft={190} marginTop={150}>
+     <Image w={"80%"} src='https://seosherpa.com/wp-content/uploads/2020/12/seo-sherpa-404-page.png' alt="not found"/>
+    </Box>
   );
 }
 
